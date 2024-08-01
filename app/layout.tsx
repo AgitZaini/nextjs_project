a68ed801f6e53c9517c7fda66466e7c4821510c5
Agit Zaini Hidayat <agitzaini@Agits-MacBook-Pro.local>
2024-08-01 17:00:18 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function RootLayout({ children }: {  children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body> 
     </html>
   );
 }
